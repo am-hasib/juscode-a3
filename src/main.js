baec34bf0511faa2cd929491c23e6d255f510ecb
am-hasib <amhasiv2014@gmail.com>
2024-02-09 01:57:03 +0600

    Assignment 3 Done

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import "../styles/modern-normalize.css"
+import "../styles/style.css";
+
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/components/features.css";
+import "../styles/components/pricing.css";
+import "../styles/components/network.css";
+import "../styles/components/testimonials.css";
+import "../styles/components/subscriber.css";
+import "../styles/components/footer.css";
+
+import "../styles/utils.css"
